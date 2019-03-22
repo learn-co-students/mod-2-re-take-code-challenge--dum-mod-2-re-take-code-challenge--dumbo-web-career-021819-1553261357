debf475dc040e4c80e5f48a4693b06fd2fd4f6da
github username <github email address>
2019-03-22 11:15:24 -0400

    finshed

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,34 @@
 class PizzasController < ApplicationController
+
+  before_action :get_pizza, only: [:show]
+
   def index
     @pizzas = Pizza.all
+  end
+  def show
+    pizzas = Pizza.all
+  end
+
+  def new
+    @pizzas = Pizza.all
+    @pizza = Pizza.new
+  end
+  def create
+    @pizza = Pizza.create(pizza_params)
+    if @pizza.valid?
+      redirect_to @pizza
+    else
+      flash[:error] = @pizza.errors.full_messages
+      redirect_to new_pizza_path
+    end
+  end
+  private
+
+  def get_pizza
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_id)
   end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,12 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    @pizza = Pizza.find(params[:id])
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @pizzas = Pizza.all
+  end
+
 end
